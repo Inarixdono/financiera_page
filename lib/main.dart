ee317a6d3b36e9d96ba12d670202128fdfdf1014
Apa73ch <adrianfondeurfdz@gmail.com>
2024-11-23 18:00:39 -0400

    First commit

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'views/prestamo_calculadora.dart';
+import 'routes.dart';
+
+void main() {
+  runApp(const MyApp());
+}
+
+class MyApp extends StatelessWidget {
+  const MyApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'FinanTech',
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.blue),
+        useMaterial3: true,
+      ),
+      initialRoute: AppRoutes.home,
+      routes: AppRoutes.routes,
+    );
+  }
+}
---
+++ lib/views/servicios_screen.dart
@@ -0,0 +1,19 @@
+import 'package:financiera_pagina/widgets/app_bar_actions.dart';
+import 'package:financiera_pagina/widgets/navigation_drawer.dart';
+import 'package:flutter/material.dart';
+
+class Servicios extends StatelessWidget {
+  const Servicios({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    final bool isDesktop = MediaQuery.of(context).size.width >= 600;
+    return Scaffold(
+        appBar: AppBar(
+          title: const Text('FinanTech'),
+          actions: isDesktop ? const [AppBarActions()] : null,
+        ),
+        drawer: isDesktop ? null : const CustomNavigationDrawer(),
+        body: const Text("Aquí diremos quienes somos"));
+  }
+}
